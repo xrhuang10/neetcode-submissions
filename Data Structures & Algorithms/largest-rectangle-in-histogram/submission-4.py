eab5ede730b6375384ea0bafd1b15ea9646f45ae
Xirui Huang <xrhuang10@gmail.com>
2026-05-23 20:47:53 -0400

    Add: largest-rectangle-in-histogram - submission-4

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        maximum = 0
+
+        for i in range(len(heights) + 1):
+            start = i
+            while stack and (i == len(heights) or heights[i] < stack[-1][1]):
+                index, maxheight = stack.pop()
+                maximum = max(maximum, maxheight * (i - index))
+                start = index
+            
+            stack.append((start, heights[i] if i < len(heights) else 1000))     
+        
+        return maximum
+
